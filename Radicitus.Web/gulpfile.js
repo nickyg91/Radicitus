09a114de6023f4d300e8d2bcac0e4fec555c0f33
Nick Ganter <nickganter91@gmail.com>
2017-12-21 23:52:30 -0600

    npm/gulp packing set up, initial page and starting text is there, also fuck gulp

---
+++ Radicitus.Web/gulpfile.js
@@ -0,0 +1,48 @@
+﻿/// <binding BeforeBuild='css:copy, scripts:copy' AfterBuild='css:copy, scripts:copy' />
+"use strict";
+
+var gulp   = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify"),
+    copy   = require("gulp-copy");
+    
+
+var paths = {
+    webroot: "./wwwroot/",
+    lib: "./wwwroot/lib/",
+    nodeRoot: "./node_modules/"
+};
+
+paths.bootstrap = paths.nodeRoot + "bootstrap/dist/js/bootstrap.js";
+paths.jquery = paths.nodeRoot + "jquery/dist/jquery.js";
+paths.bootbox = paths.nodeRoot + "bootbox/bootbox.js";
+paths.jqvalidation = paths.nodeRoot + "jquery-validation/dist/jquery.validate.js";
+paths.jqvalidationunob = paths.nodeRoot + "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js";
+paths.unobtrusiveAjax = paths.nodeRoot + "jquery-ajax-unobtrusive/jquery.unobtrusive-ajax.js";
+paths.bootstrapCssPath = paths.nodeRoot + "bootstrap/dist/css/bootstrap.css";
+paths.popper = paths.nodeRoot + "popper.js/dist/umd/popper.js";
+
+gulp.task("libs:clean",
+    function(cb) {
+        rimraf(paths.lib, cb);
+    });
+
+gulp.task("scripts:copy",
+    function() {
+        gulp.src([paths.bootstrap,
+            paths.bootbox,
+            paths.jquery,
+            paths.jqvalidation,
+            paths.jqvalidationunob,
+            paths.unobtrusiveAjax,
+            paths.popper])
+            .pipe(gulp.dest(paths.lib));
+    });
+
+gulp.task("css:copy",
+    function() {
+        gulp.src([paths.bootstrapCssPath])
+            .pipe(gulp.dest(paths.lib));
+    });
