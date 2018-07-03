3f5b791fa00e96f33a165f04f1d207872b03ec3a
Nick Ganter <nickganter91@gmail.com>
2018-07-03 17:54:43 -0500

    Added authentication scheme to admin controller.

--- Radicitus.Web/Controllers/AdminController.cs
+++ Radicitus.Web/Controllers/AdminController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Radicitus.Entities;
 using Radicitus.SqlProviders;
 
 namespace Radicitus.Web.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private readonly IRadSqlProvider _adminRepo;
